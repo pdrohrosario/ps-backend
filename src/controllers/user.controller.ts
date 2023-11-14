a747e87b743aaa274ec06f5f5c6280df2a2cdb0d
Alan Nascimento <alanmnasci@gmail.com>
2023-11-14 20:17:45 -0300

    First

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 import { UserCreationDTO } from 'src/dtos/user-creation-dto';
 import { UserService } from 'src/services/user.service';
 
@@ -11,15 +11,15 @@ export class UserController {
   async createUser(
     @Body() userData: UserCreationDTO,
   ): Promise<User> {
-    const { 
+    const {
       email,
       name,
       profile,
       children,
       feedback_frequence,
-      password 
+      password
     } = userData;
-    
+
     return this.userService.createUser({
       email,
       name,
@@ -29,4 +29,17 @@ export class UserController {
       password,
     });
   }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: {
+      where: Prisma.UserWhereUniqueInput;
+      data: Prisma.UserUpdateInput;
+    },
+  ): Promise<any> {
+    const { where, data } = updateUserDto;
+    return this.userService.updateUser({ where, data });
+  }
+
 }
